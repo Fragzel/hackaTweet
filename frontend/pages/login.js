1838a6039496784bdf1a4d0d28535d23aec3cb81
Oscar <fragzel@gmail.com>
2024-07-18 11:43:27 +0200

    ok

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,12 +1,41 @@
 import styles from '../styles/Login.module.css';
+import LoginModal from '../components/LoginModal';
+import { useState } from 'react';
 
 function Login() {
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [isSignin, setIsSignin] = useState(false)
+
+    function openModal(isSignin) {
+        setIsSignin(isSignin)
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+
     return (
         <div>
             <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Next.js!</a>
-                </h1>
+
+                <div className={styles.leftPart}>
+
+                </div>
+
+                <div className={styles.rightPart}>
+                    <img className={styles.twitterLogo} src='/images/logo_twitter.png' />
+                    <h1 className={styles.title}>See what's happening</h1>
+                    <h3 className={styles.text}>Join Hackatweet today</h3>
+                    <div className={styles.sign}>
+                        <button onClick={() => openModal(true)}>signin</button>
+                        <span>Already have an account ?</span>
+                        <button onClick={() => openModal(false)} >signup</button>
+                    </div>
+                </div>
+                <LoginModal closeModal={closeModal} modalIsOpen={modalIsOpen} isSignin={isSignin} />
+
             </main>
         </div>
     );
