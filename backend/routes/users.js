d33187652ecebe651ecc5626ea5c100a010a337e
Heymow <lynckit@hotmail.com>
2024-07-18 10:13:44 +0200

    squelette de routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+// SIGN UP
+router.post('/signup', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+// SIGN IN
+router.post('/signin', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+// LOG OUT
+router.post('/logout', function (req, res, next) {
   res.send('respond with a resource');
 });
 
