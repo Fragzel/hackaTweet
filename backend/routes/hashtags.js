4f0c863df6a96cf2d7cf3ac20d18c46b7f9b6cd1
Heymow <lynckit@hotmail.com>
2024-07-18 18:01:23 +0200

    save

--- backend/routes/hashtags.js
+++ backend/routes/hashtags.js
@@ -1,21 +1,40 @@
 
 var express = require('express');
 var router = express.Router();
+const Hashtag = require('../models/hashtags')
+
+const { checkBody } = require('../modules/tools');
 
 require('../models/connection');
 
 
-// VIEW ALL POST WITH THIS # A tester, piti doute
-router.get("/hashtags/:hashtag", async (req, res) => {
+// VIEW ALL POST WITH THIS HASHTAG
+router.get("/all", async (req, res) => {
 
-    const foundHashtagsPosts = await Hashtag.find({ name: req.params.hashtag })
+    const foundHashtagsPosts = await Hashtag.find({ name: req.body.hashtag })
     if (foundHashtagsPosts.length) {
-        res.send({ result: true, allHashtagsPost: foundHashtagsPosts })
+        res.send({ result: true, allHashtagPosts: foundHashtagsPosts })
     } else {
         res.send({ result: false, error: "Aucun post trouvé avec ce Hashtag ! " })
     }
 });
 
+
+// ADD NEW HASHTAG
+router.post("/add", async (req, res) => {
+    if (!checkBody(req.body, ['hashtag'])) {
+        res.json({ result: false, error: 'Missing or empty fields' });
+        return;
+    }
+    const foundHashtag = await Hashtag.findOne({ name: req.body.hashtag });
+    if (!foundHashtag) {
+        const newHashtag = new Hashtag({ name: req.body.hashtag, posts: [] })
+        newHashtag.save();
+        res.json({ result: true, foundHashtag })
+    } else {
+        res.json({ result: true, message: 'added to hashtag' })
+    }
+});
 
 
 module.exports = router;
