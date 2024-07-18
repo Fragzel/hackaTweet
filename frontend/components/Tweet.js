efe1fd928f2c3c52f7ef986fa572ddefa4467bdb
Oscar <fragzel@gmail.com>
2024-07-18 16:27:27 +0200

    tweet card

--- frontend/components/Tweet.js
+++ frontend/components/Tweet.js
@@ -1,13 +1,22 @@
 import styles from '../styles/Tweet.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
 
 function Tweet() {
 
     return (
-        <div>
-            <main className={styles.main}>
+        <div className={styles.tweetContainer}>
+            <div className={styles.imageAndUsername}>
+                <img className={styles.avatar} src="/images/Khaled.jpg" />
+                <span className={styles.name}>Khaled</span> <span className={styles.arobase}>@khaledLeBoss</span>
 
-
-            </main>
+            </div>
+            <div className={styles.messageAndHeart} >
+                <span>Message raciste random </span>
+            </div>
+            <FontAwesomeIcon className={styles.faHeart} icon={faHeart} /> 0
         </div>
     );
 }
